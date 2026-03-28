6255b8ade7f39a8cf5406734bcadf7a5655d08dd
Jakub Polak <s27999@pjwstk.edu.pl>
2026-03-28 20:37:08 +0100

    Almost finished Logic and added Service

--- Logic/DataManager.cs
+++ Logic/DataManager.cs
@@ -1,4 +1,5 @@
 ﻿using APBD_CW2.Database;
+using APBD_CW2.Models;
 
 namespace APBD_CW2.Logic;
 
@@ -10,5 +11,59 @@ public class DataManager
     public DataManager(AppDatabase database)
     {
         this._appDatabase = database;
+    }
+
+    public List<Equipment> getAllEquipment()
+    {
+        return _appDatabase.Equipment;
+    }
+
+    public List<Person> getAllPersons()
+    {
+        return this._appDatabase.Persons;
+    }
+
+    public List<RentEquipment> getAllRentEquipment()
+    {
+        return this._appDatabase.RentEquipment;
+    }
+    
+    public List<Equipment> getAvailableEquipment()
+    {
+        List<Equipment> availableEquipment = new List<Equipment>();
+        foreach (Equipment e in _appDatabase.Equipment)
+        {
+            if (e.Availability == EquipmentStatus.Dostepny)
+            {
+                availableEquipment.Add(e);   
+            }
+        }
+        return availableEquipment;
+    }
+
+    public List<RentEquipment> getPersonRent(Person person)
+    {
+        List<RentEquipment> personRent = new List<RentEquipment>();
+        foreach (RentEquipment rentEquipment in _appDatabase.RentEquipment)
+        {
+            if (rentEquipment.PersonId == person.Id)
+            {
+                personRent.Add(rentEquipment);
+            }
+        }
+        return personRent;
+    }
+
+    public List<RentEquipment> getOverdueRent()
+    {
+        List<RentEquipment> overdueRent = new List<RentEquipment>();
+        foreach (RentEquipment rentEquipment in _appDatabase.RentEquipment)
+        {
+            if (rentEquipment.PlannedReturnDate < DateOnly.FromDateTime(DateTime.Now))
+            {
+                overdueRent.Add(rentEquipment);
+            }
+        }
+        return overdueRent;
     }
 }
